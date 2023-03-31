303c7de87d0c2ccb65adf15c0dc8af310d7bd8a9
unknown <jayceb.ham17@yahoo.com>
2023-03-30 21:39:54 -0600

    I started the calculator finised the html and css did some of the JavaScript

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,49 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement[0]
+        this.currentOperandTextElement = currentOperandTextElement[0]
+        this.clear()
+    }
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+    delete() {
+
+    }
+    appendNumber(number) {
+        this.currentOperand += number
+    }
+    chooseOperation(operation) {
+
+    }
+    compute() {
+
+    }
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand + ' ' + this.operation
+        console.log('working')
+    }
+}  
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButtons = document.querySelectorAll('[data-equals]')
+const deleteButtons = document.querySelectorAll('[data-delete]')
+const allClearButton = document.querySelectorAll('[data-all-clear]')
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()  
+        
+    })
+})
+
 // To do list:
 var addItemButton = document.querySelector("#add-item-btn");
 var todoList = document.querySelector("#todo-list");
