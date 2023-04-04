d2c87445e6778cf45b57f35415b5ee707b14899b
unknown <jayceb.ham17@yahoo.com>
2023-04-03 20:25:36 -0600

    I finishing up the javascript everythingis working except the delete button

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,30 +7,59 @@ class Calculator {
     clear() {
         this.currentOperand = ''
         this.previousOperand = ''
-        this.operation = undefined
+        this.operation = ''
     }
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
     appendNumber(number) {
-        this.currentOperand += number
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     }
     chooseOperation(operation) {
-
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
     compute() {
-
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev +  current
+                break
+            case '-':
+                computation = prev -  current
+                break
+            case '*':
+                computation = prev *  current
+                break
+            case '÷':
+                computation = prev /  current
+                break
+            default: 
+            return
+        }
+        this.currentOperand = computation
+        this.operation = ''
+        this.previousOperand = ''
     }
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand + ' ' + this.operation
-        console.log('working')
+        this.currentOperandTextElement.innerText = this.currentOperand + '' + this.operation
+        this.previousOperandTextElement.innerText = this.previousOperand + '' + this.operation
     }
 }  
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButtons = document.querySelectorAll('[data-equals]')
-const deleteButtons = document.querySelectorAll('[data-delete]')
-const allClearButton = document.querySelectorAll('[data-all-clear]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.getElementById('data-delete')
+const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
 const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
 
@@ -42,6 +71,29 @@ numberButtons.forEach(button => {
         calculator.updateDisplay()  
         
     })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()  
+        
+    })
+})
+
+equalsButton.addEventListener('click', () => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', () => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete()
+    calculator.updateDisplay()
 })
 
 // To do list:
