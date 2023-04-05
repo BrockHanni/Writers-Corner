427d698a7edcb08affe63491c949eb6ec2405635
BrockHanni <hannibr26@gmail.com>
2023-04-04 19:00:21 -0600

    weather card added

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -62,3 +62,40 @@ treebutton.addEventListener("click",() => {
 winterbutton.addEventListener("click",() => {
   body.style.backgroundImage= 'url("./assets/Images/Winter.gif")'
 })
+
+// Weather API Card
+
+//api key
+var APIKey = "daf6739626e4defe871d6e34398cd5af"
+// search button info
+var searchInput = document.getElementById("searchvalue");
+var searchButton = document.getElementById("searchbtn");
+searchButton.addEventListener("click", function(event){
+  var city = searchInput.value;
+  var weatherurl = ("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial")
+  event.preventDefault()
+
+  fetch(weatherurl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+        var cityNameEl= document.getElementById("cityName")
+        cityNameEl.innerHTML= (data.name)
+        var cityDataCardEl= document.getElementById("cityDataCard")
+        cityDataCardEl.innerHTML= ("Weather: " + data.weather[0].description)
+        var cityDataCardTemp= document.getElementById("cityDataCardTemp")
+        cityDataCardTemp.innerHTML= ("Temp: " + data.main.temp + "°F")
+        var cityDataCardHumid= document.getElementById("cityDataCardHumid")
+        cityDataCardHumid.innerHTML = ("Humidity: " + data.main.humidity)
+        var cityDataCardWind= document.getElementById("cityDataCardWind")
+        cityDataCardWind.innerHTML = ("Wind: " + data.wind.speed + "mph")
+    })
+    .catch(error => {
+      console.log(error)
+    });
+
+  var weatherInfo = document.getElementById("weatherInfo")
+  weatherInfo.classList.remove("hidden")
+  var weatherCity = document.getElementById("weatherCity")
+  weatherCity.classList.add("hidden")
+});
